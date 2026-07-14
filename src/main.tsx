0babbf98f14150267bad76412be9195482b768ab
REKAA_85 <ignite855two@gmail.com>
2026-07-14 04:11:53 +0000

    Init

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.scss'
 import App from './App.tsx'
 
+// Restore the real path after the GitHub Pages 404.html SPA redirect (see public/404.html).
+const redirect = new URLSearchParams(location.search).get('redirect')
+if (redirect) {
+  history.replaceState(null, '', import.meta.env.BASE_URL.replace(/\/$/, '') + redirect)
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
